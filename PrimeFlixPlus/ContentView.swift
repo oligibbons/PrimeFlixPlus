537f368c6ce62511f926e819b89ac7b5462ad9d7
oligibbons <olipg@hotmail.co.uk>
2025-12-02 16:04:21 +0000

    Initial Commit

---
+++ PrimeFlixPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrimeFlixPlus
+//
+//  Created by Oliver Perkins-Gibbons on 02/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PrimeFlixPlus/PrimeFlixPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeFlixPlusApp.swift
+//  PrimeFlixPlus
+//
+//  Created by Oliver Perkins-Gibbons on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct PrimeFlixPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
